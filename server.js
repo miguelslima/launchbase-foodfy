7d0c4d7cd019b24f63ec420e94e78dc6acadd724
Miguel Lima <miguelsousalima@hotmail.com>
2020-03-01 03:37:53 -0300

    Error page index

--- server.js
+++ server.js
@@ -42,9 +42,9 @@ server.get("/recipes/:index", function (req, res) {
   return res.render('receipt', { item: recipe })
 })
 
-server.use(function (req, res) {
-  res.status(404).render("not-found")
-})
+// server.use(function (req, res) {
+//   res.status(404).render("not-found")
+// })
 
 server.listen(5000., function () {
   console.log("Server is running");
